656fe4f9b709f0d3e256a610ae28ae950d705e06
Misa <misaelfernandes2023@gmail.com>
2024-10-22 10:25:51 -0300

    fix

---
+++ src/models/laudo.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const LaudoSchema = new mongoose.Schema({
+  data: {
+    type: String,
+    required: true,
+  },
+  crm: {
+    type: String,
+    required: true,
+  },
+  texto: {
+    type: String,
+    required: true,
+  },
+  arquivo: {
+    type: Buffer,
+    required: true,
+  },
+}, {
+  timestamps: true, // Para adicionar campos de criação e atualização automática
+});
+
+module.exports = mongoose.model('Laudo', LaudoSchema);
